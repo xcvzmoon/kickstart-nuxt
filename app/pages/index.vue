795f5a28d620c4dd9572ece69c60f779dfee0f97
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2025-11-06 21:31:58 +0800

    chore: update index page content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,27 @@
 <template>
-  <p>Welcome</p>
+  <section class="flex h-svh flex-col items-center justify-center gap-2">
+    <UButton
+      icon="i-lucide-github"
+      to="https://github.com/xcvzmoon"
+      class="size-8 justify-center"
+    />
+
+    <USeparator class="w-48"> Kickstarters </USeparator>
+
+    <UButton
+      color="secondary"
+      variant="subtle"
+      to="https://github.com/xcvzmoon/kickstart-nuxt"
+      label="Nuxt Kickstarter (Frontend)"
+      trailing-icon="i-lucide-external-link"
+    />
+
+    <UButton
+      color="secondary"
+      variant="subtle"
+      to="https://github.com/xcvzmoon/kickstart-nitro"
+      label="Nitro Kickstarter (Backend)"
+      trailing-icon="i-lucide-external-link"
+    />
+  </section>
 </template>
