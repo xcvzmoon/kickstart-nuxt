e90fb5fe67a45cb9012af5644db6bfef337d180e
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-01-25 13:11:02 +0800

    chore: update config and packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,29 @@
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: 'latest',
+  compatibilityDate: '2026-01-25',
+
   devtools: {
     enabled: true,
   },
+
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       include: ['vue', 'vue-router'],
     },
   },
+
   css: ['~/assets/css/main.css'],
+
   modules: ['@nuxt/eslint', '@nuxt/ui', '@vueuse/nuxt', '@nuxt/hints', '@nuxt/test-utils'],
+
   colorMode: {
     preference: 'dark',
     fallback: 'dark',
+  },
+
+  routeRules: {
+    '/': { prerender: true },
   },
 });
