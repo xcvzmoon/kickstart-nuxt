d92fdd15885286c264bb39962fb6f7d1dad77e2f
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-03-21 10:41:42 +0800

    chore: bump nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,25 @@
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: '2026-01-25',
-
+  compatibilityDate: '2026-03-21',
   devtools: {
     enabled: true,
   },
-
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
-      include: ['vue', 'vue-router'],
+      include: ['vue', 'vue-router', '@vue/devtools-core', '@vue/devtools-kit'],
     },
   },
-
   css: ['~/assets/css/main.css'],
-
   modules: ['@nuxt/ui', '@vueuse/nuxt', '@nuxt/hints', '@nuxt/test-utils'],
-
   colorMode: {
     preference: 'dark',
     fallback: 'dark',
   },
-
   routeRules: {
-    '/': { prerender: true },
+    '/': {
+      prerender: true,
+    },
   },
 });
