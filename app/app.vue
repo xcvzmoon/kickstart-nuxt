52f31c3fdea40b39c8500d3b3d5072204bba5c4f
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-04-11 17:58:59 +0800

    chore: move head to nuxt config

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-  useHead({
-    meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-    link: [{ rel: 'icon', href: '/favicon.ico' }],
-    htmlAttrs: { lang: 'en' },
-  });
-
   const title = 'Kickstart Nuxt';
   const description = 'My nuxt starter template.';
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,4 +22,24 @@ export default defineNuxtConfig({
       prerender: true,
     },
   },
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          href: '/favicon.ico',
+        },
+      ],
+      htmlAttrs: {
+        lang: 'en',
+      },
+      title: 'Kickstart Nuxt',
+    },
+  },
 });
