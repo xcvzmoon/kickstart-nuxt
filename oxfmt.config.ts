3049ad29ee40a3bb8f037816e23eb9d58ddff121
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-05-14 14:07:02 +0800

    chore(oxc-config): switch from json to ts

---
+++ oxfmt.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+  sortImports: {
+    groups: [
+      'type-import',
+      'type-internal',
+      ['type-parent', 'type-sibling', 'type-index'],
+      ['value-builtin', 'value-external'],
+      'value-internal',
+      ['value-parent', 'value-sibling', 'value-index'],
+      'unknown',
+    ],
+  },
+  sortTailwindcss: {
+    stylesheet: './app/assets/css/main.css',
+    attributes: ['class'],
+    functions: ['clsx', 'cn'],
+    preserveWhitespace: true,
+  },
+  ignorePatterns: [
+    'node_modules',
+    'dist',
+    'pnpm-lock.yaml',
+    'CHANGELOG.md',
+    '.DS_Store',
+    '.env*',
+    '.vscode',
+  ],
+  sortPackageJson: false,
+  singleQuote: true,
+  singleAttributePerLine: true,
+  vueIndentScriptAndStyle: true,
+});
